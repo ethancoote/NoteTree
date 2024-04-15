1438dee601eefebcc97f8caf515c0d54f9faf044
ecoote <ecoote@uoguelph.ca>
2024-04-15 19:21:49 -0400

    adding note window. opens on new note button click

--- NoteWeb/Note.xaml.cs
+++ NoteWeb/Note.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace NoteWeb
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Note.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Note : Window
     {
-        public MainWindow()
+        public Note()
         {
             InitializeComponent();
-        }
-
-        private void NewNote_Click(object sender, RoutedEventArgs e)
-        {
-            Note note = new Note();
-            note.Show();
         }
     }
 }
